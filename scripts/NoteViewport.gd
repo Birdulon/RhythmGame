ea1ed1fa17a168223fbb2d4cf554769836c826a3
Luke Hubmayer-Werner <mongoose41@gmail.com>
2021-09-19 19:07:13 +0930

    Fix viewport size not updating correctly in many cases for subsampling

--- scripts/NoteViewport.gd
+++ scripts/NoteViewport.gd
@@ -1,5 +1,8 @@
 extends Viewport
 
+export var SquarePath := @'../../'
+onready var Square := get_node(SquarePath)
+onready var Root := $'/root'
 onready var base_height = 1080.0
 
 var container_size := Vector2(1080, 1080)
@@ -10,17 +13,15 @@ func set_render_scale(scale: Vector2):
 	$Center.position = size * 0.5
 	$Center.scale = size/base_height
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Settings.connect('subsampling_changed', self, 'set_render_scale')
-	set_render_scale(Settings.subsampling)
-	_on_Square_item_rect_changed()
-
-
-onready var Square := $'../../'
-onready var Root := $'/root'
-onready var Main := $'/root/main'
-func _on_Square_item_rect_changed() -> void:
+func update_size() -> void:
 	var winscale = min(Root.size.x, Root.size.y)/base_height
 	container_size = Square.rect_size * winscale
 	set_render_scale(scale)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Settings.connect('subsampling_changed', self, 'set_render_scale')
+	Square.connect('item_rect_changed', self, 'update_size')
+	Root.connect('size_changed', self, 'update_size')
+	scale = Settings.subsampling
+	update_size()
